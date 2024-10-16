6b7cbb9aa53cd88c0ddd252d4d68a364cf962a15
Andres Arcila <andresarcilaa2@me.com>
2024-10-16 16:01:20 -0500

    daisy ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,16 @@
 import { type Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  plugins: [
+    daisyui
+  ],
+  daisyui: {
+    themes: [
+      "lemonade"
+    ],
+  },
 } satisfies Config;
